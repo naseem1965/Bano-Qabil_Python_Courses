7c26599467bb66f21832a5eaf9dcf6381416670e
naseem1965 <156215223+naseem1965@users.noreply.github.com>
2024-01-27 21:13:40 +0500

    Add files via upload

---
+++ pratice.py
@@ -0,0 +1,104 @@
+#Asssignment 2:
+
+#Task 1:
+#Declare two variables, num1 and num2, with any integer values. Use these to calculate their sum :
+
+FirstNumber=int(input("Enter Your First No:"))
+SecondNumber=int(input("Enter Your Second No:"))
+Sum=FirstNumber+SecondNumber
+print("Sum 10+10 : ",Sum)
+
+#End Task
+
+#Task2:
+#Create a variable called message and give it a string value. Append the string " World!" to it :
+
+message=["Hello"]
+message.append("World")
+str(message)
+print("Message is :",message)
+
+#End Task
+
+#Task3:
+#Declare a variable, is_python_fun, and assign it a boolean value :
+
+is_python_fun=True
+print("is_python_fun :",is_python_fun)
+
+#End Task
+
+#Task4:
+#Create a list called fruits with at least three different fruit names :
+
+list=["Apple","Mango","banana","Orange"]
+print("Your Fruits List is:",list)
+list.append("Kiwi")
+print("Your Updated Fruits List is:",list)
+
+#End Task
+
+#Task5:
+#Declare a variable called price with a floating-point value,Convert it to an integer :
+
+price=float(80)
+print("Price is:",price)
+print("Updated Price is:",int(price))
+
+#End Task
+
+#Task6:
+#Create a dictionary named student_info with keys for 'name', 'age', and 'grade':
+
+student_info={"Name:"'A.Rahmnan',"Age:"'20',"Grade:"'A*'}
+print("This Student Info is:",student_info)
+
+#End Task
+
+#Task7:
+#Write a program that takes user input for their age and prints a message addressing their age group :
+
+UserInput=int(input("Enter your Age:"))
+if UserInput<19 :
+    print(UserInput,":You Are In Your Young Age")
+if UserInput<39 :
+    print(UserInput,":You Are In Your Younger Age")
+
+if UserInput >= 40 :
+    print(UserInput,":You Are In Your Adult Age")
+
+#End Task
+
+#Task8:
+#Create a complex number variable, comp_num, with real and imaginary parts,Print both parts separately :
+
+comp_realnum=int(3)
+comp_Imaginarynum= str("bi")
+complexNumber=str("3+bi")
+print("complex Number is:",complexNumber)
+print("comp_real num is:",comp_realnum)
+print("comp_Imaginary num is:",comp_Imaginarynum)
+
+#End Task
+
+#Task9:
+#Combine two strings using concatenation :
+
+firststring=str("Abdur Rahman")
+secondstring=str("Naseem")
+print("The Combination of two String's is:",firststring+" "+secondstring)
+
+#End Task
+
+#Task10:
+#Create a tuple named days_of_week with the names of the days,Access and print the third day of the week :
+
+
+Daysinaweek=('Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday')
+print("Third Day in a Week is:",Daysinaweek[2])
+
+#End Task
+
+
+
+
